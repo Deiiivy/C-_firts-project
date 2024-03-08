a5f356f8de8a89a655d7b64767cdb8f8372d53de
Deiiivy <deivypr28@gmail.com>
2024-03-07 21:06:34 -0500

    inicializando proyecto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Sale sale = new Sale(15);
+var message = sale.GetInfo();
+
+
+Console.WriteLine(message);
+class Sale {
+    public decimal Total {get; set;}
+
+    public Sale(decimal Total)
+    {
+        this.Total = Total;
+    }
+
+    public String GetInfo() {
+        return "el total es " + Total;
+    }
+}
